2822f3c4eea7afbec892cdc2e43e5f5b22419d80
Praful Divani <40952264+PrafulDiwani@users.noreply.github.com>
2021-04-05 08:54:11 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const boodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const port = 5000;
+
+// Middle wear
+app.use(bodyParser.json());
 
 const mockUserData = [
   {
@@ -19,11 +24,31 @@ app.get("/users", function (req, res) {
 
 app.get("/user/:id", function (req, res) {
   res.json({
-    success: true ,
-    message :  'got one user',
-    user : req.params.id  
-  })
-})
+    success: true,
+    message: "got one user",
+    user: req.params.id,
+  });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = "praful";
+  const mockPassword = "admin";
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: "password and username match",
+      token: "encrypted toek goes hear",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and username do not match",
+    });
+  }
+});
 
 app.listen(port, function () {
   console.log(`server listen at ${port}`);
