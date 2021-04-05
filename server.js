cb25b37ea1dd19ff4da1c5a94d347d9c9f91dcd7
Praful Divani <40952264+PrafulDiwani@users.noreply.github.com>
2021-04-05 08:35:58 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require("express")
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
+const mockUserData = [
+  {
+    name: "mark",
+    name: "jill",
+  },
+];
 
-
+app.get("/users", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users, Nice!",
+    users: mockUserData,
+  });
+});
 
 app.listen(port, function () {
-  console.log(`server listen at ${port}`)  
-})
+  console.log(`server listen at ${port}`);
+});
